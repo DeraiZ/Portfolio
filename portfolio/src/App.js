eac56e0e1bee094d853d8b056fdc4fabce9ee3a2
DeraiZ <91881453+DeraiZ@users.noreply.github.com>
2023-06-11 16:51:42 +0200

    modify head and icon

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Portfolio from "./Page/Portolio";
+import {useEffect} from "react";
 
 
 function App() {
-
+  useEffect(() => {
+    document.title = "DeraiZ Portfolio";
+  }, []);
   return (
     <Portfolio/>
   );
